d4f0c6a1c480985d9eb23deb228bcf46dfca7a21
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-02 21:56:35 -0300

    POST /game

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -9,3 +9,24 @@ export async function getGames(req,res){
         res.status(500).send(error.message)
     }
 }
+
+export async function postGames(req,res){
+    const { name, image, stockTotal, pricePerDay } = req.body
+
+    res.sendStatus(201)
+    try {
+        const game = await db.query(`SELECT name FROM game WHERE name = $1`, [name])
+        if ( game.rowCount > 0 )
+            return res.status(409).send({message:'Jogo já cadastrado!'});
+
+            const result = db.query(`
+            INSERT INTO games(name, image, "stockTotal", "pricePerDay")
+                VALUES($1, $2, $3, $4);
+            `, [name, image, stockTotal, pricePerDay]);
+            
+            res.status(201).send(result)
+        
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
