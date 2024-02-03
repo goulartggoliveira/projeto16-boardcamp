7f4ee599c293a85a93b928ed616ffa1322735b25
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-03 13:46:00 -0300

    POST /game validation

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -15,14 +15,24 @@ export async function postGames(req,res){
     const { name, image, stockTotal, pricePerDay } = req.body
 
     try {
-                
+        const duplicateNameCheck = await db.query(`
+        SELECT COUNT(*) AS count
+        FROM games
+        WHERE name = $1;
+    `, [name]);
+
+    const isDuplicateName = duplicateNameCheck.rows[0].count > 0;
+
+    if (isDuplicateName) {
+        res.status(409).send("Duplicate name. Please choose a different name.");
+    } else {      
             await db.query(`
             INSERT INTO games (name, image, "stockTotal", "pricePerDay")
                 VALUES($1, $2, $3, $4);
             `, [name, image, stockTotal, pricePerDay]);
 
             res.sendStatus(201)
-        
+    }
     } catch (error) {
         res.status(500).send(error.message)
     }
