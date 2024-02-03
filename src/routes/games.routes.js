1b0dc6736b6524a9eebe63539eea767c4b794503
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-02 22:13:33 -0300

    POST /game bug correction route

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import validateSchema from "../middlewares/vs.middleware.js";
 import gameSchema from "../schemas/games.schemas.js";
-import { getGames } from "../controllers/games.controllers.js";
+import { getGames, postGames } from "../controllers/games.controllers.js";
 
 
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames)
-gamesRouter.post("/games", validateSchema(gameSchema),)
+gamesRouter.post("/games", validateSchema(gameSchema), postGames)
 
 export default gamesRouter
