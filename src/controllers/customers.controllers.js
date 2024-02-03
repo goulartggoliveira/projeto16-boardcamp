c35fc71c0443abdede08056696dc363457cfc090
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-03 14:34:58 -0300

    POST /customers

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -37,3 +37,21 @@ export async function getCustomersId(req,res){
         
     }
 }
+
+export async function postCustomers(req,res){
+    const { name, phone, cpf, birthday } = req.body
+
+    try {
+        
+        await db.query(`
+            INSERT INTO customers(name, phone, cpf, birthday)
+            VALUES( $1, $2, $3, $4);
+        `, [name, phone, cpf, birthday]);
+
+        res.sendStatus(201);
+
+
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { getCustomers, getCustomersId } from "../controllers/customers.controllers.js";
+import { getCustomers, getCustomersId, postCustomers } from "../controllers/customers.controllers.js";
+import validateSchema from "../middlewares/vs.middleware.js";
+import { customerSchema } from "../schemas/customers.schemas.js";
 
 
 
@@ -7,7 +9,7 @@ const customersRoutes = Router()
 
 customersRoutes.get("/customers", getCustomers)
 customersRoutes.get("/customers/:id", getCustomersId)
-customersRoutes.post("/customers")
+customersRoutes.post("/customers",validateSchema(customerSchema), postCustomers)
 customersRoutes.put("customers/:id")
 
 export default customersRoutes
