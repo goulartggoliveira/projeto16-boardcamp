a29ed8cd48290edaf75e14638a41efd6a476ef9a
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-03 17:58:02 -0300

    POST /rentals

---
+++ src/controllers/rentals.controllers.js
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+import { db } from "../database/database.connection.js";
+
+export async function postRentals(req,res){
+
+    const { customerId, gameId, daysRented } = req.body;
+
+        try {
+
+            const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [customerId])
+        if (customer.rowCount === 0) return res.status(400).send("User not found")
+
+        const game = await db.query(`SELECT * FROM games WHERE id=$1;`, [gameId])
+        if (game.rowCount === 0) return res.status(400).send("Game not found!")
+
+        const checkGameStock = await db.query(`
+            SELECT * FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL;`, [gameId])
+
+        if (checkGameStock.rowCount >= game.rows[0].stockTotal) {
+            return res.status(400).send("Game not available")
+        }
+
+        await db.query(`
+            INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee")
+            VALUES ($1, $2, $3, $4, $5, NULL, NULL);
+        `, [customerId, gameId, daysRented, dayjs().format("YYYY-MM-DD"), daysRented * game.rows[0].pricePerDay])
+
+        res.sendStatus(201)
+            
+        } catch (error) {
+            res.status(500).send(error.message)
+        }
+}
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { postRentals } from "../controllers/rentals.controllers.js";
 
 
 const rentalsRouter = Router()
 
 rentalsRouter.get("/rentals")
-rentalsRouter.post("/rentals")
+rentalsRouter.post("/rentals", postRentals)
 rentalsRouter.post("/rentals/:id/return")
 rentalsRouter.delete("/rentals/:id")
 
