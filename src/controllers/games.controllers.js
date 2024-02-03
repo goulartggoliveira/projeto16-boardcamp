ce7258f9f640c26abe5409927658145614c6888f
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-02 23:16:16 -0300

    POST /game errors(n sei mais oq pode ser)

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -15,10 +15,7 @@ export async function postGames(req,res){
     const { name, image, stockTotal, pricePerDay } = req.body
 
     try {
-        const checkGame = await db.query(`SELECT name FROM game WHERE name = $1;`, [name])
-        if ( checkGame.rowCount > 0 )
-            return res.status(409).send({message:'Jogo já cadastrado!'});
-        
+                
             await db.query(`
             INSERT INTO games (name, image, "stockTotal", "pricePerDay")
                 VALUES($1, $2, $3, $4);
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import validateSchema from "../middlewares/vs.middleware.js";
 import gameSchema from "../schemas/games.schemas.js";
 import { getGames, postGames } from "../controllers/games.controllers.js";
+import { validateCreateGame } from "../middlewares/games.middleware.js";
 
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames)
-gamesRouter.post("/games", validateSchema(gameSchema), postGames)
+gamesRouter.post("/games", validateSchema(gameSchema),validateCreateGame, postGames)
 
 export default gamesRouter
