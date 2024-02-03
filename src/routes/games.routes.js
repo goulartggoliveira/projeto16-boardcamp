53acdb7b614c6fefbab00060c0b9e00b708ceec1
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-02 23:27:06 -0300

    POST /game errors(n sei mais oq pode ser 2

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import validateSchema from "../middlewares/vs.middleware.js";
 import gameSchema from "../schemas/games.schemas.js";
 import { getGames, postGames } from "../controllers/games.controllers.js";
-import { validateCreateGame } from "../middlewares/games.middleware.js";
+
 
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames)
-gamesRouter.post("/games", validateSchema(gameSchema),validateCreateGame, postGames)
+gamesRouter.post("/games", validateSchema(gameSchema), postGames)
 
 export default gamesRouter
