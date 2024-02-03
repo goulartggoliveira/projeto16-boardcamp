073e7553a23a81415770a1c177bb8f0f798047d1
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-03 14:45:21 -0300

    POST /customers 3

--- src/schemas/customers.schemas.js
+++ src/schemas/customers.schemas.js
@@ -5,7 +5,7 @@ const joi = joiBase.extend(joiDate)
 
 export const customerSchema = joi.object({
     name: joi.string().trim().required(),
-    phone: joi.string().trim().min(10).max(11).integer().required(),
-    cpf: joi.string().trim().length(11).integer().required(),
+    phone: joi.string().trim().min(10).max(11).pattern(/^\d+$/).required(),
+    cpf: joi.string().trim().length(11).pattern(/^\d+$/).required(),
     birthday: joi.date().format(['YYYY-MM-DD']).required()
 })
