476cca8ec0423fafeebf7e29939e2ad8390942b2
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-02-05 00:33:09 -0300

    DELETE /rentals/:id 2

--- src/middlewares/rentals.middlewares.js
+++
@@ -1,9 +0,0 @@
-
-export async function rentalValidation(req, res, next) {
-    const { id } = req.params
-
-    const rental = await db.query(`SELECT * FROM rentals WHERE id=$1`, [id])
-    if (rental.rowCount === 0) return res.status(404).send({ message: "Aluguel inexistente!" })
-    if (rental.rows[0].returnDate === null) return res.status(400).send({ message: "Aluguel não pode ser deletado pois não foi finalizado." })
-    next()
-}
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { deleteRentalsId, getRentals, postRentals, rentalsIdReturn } from "../controllers/rentals.controllers.js";
 import validateSchema from "../middlewares/vs.middleware.js";
 import { rentalSchema } from "../schemas/rentals.schemas.js";
-import { rentalValidation } from "../middlewares/rentals.middlewares.js";
 
 
 const rentalsRouter = Router()
@@ -10,6 +9,6 @@ const rentalsRouter = Router()
 rentalsRouter.get("/rentals", getRentals)
 rentalsRouter.post("/rentals",validateSchema(rentalSchema), postRentals)
 rentalsRouter.post("/rentals/:id/return", rentalsIdReturn)
-rentalsRouter.delete("/rentals/:id", rentalValidation, deleteRentalsId)
+rentalsRouter.delete("/rentals/:id", deleteRentalsId)
 
 export default rentalsRouter
